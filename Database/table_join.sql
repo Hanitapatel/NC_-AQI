cea0ecf853c0262ac406682428866327c01b54c7
Spope20 <spencer.pope20@gmail.com>
2022-08-18 19:13:11 -0400

    added scheme to join the population and site reporting tables together into site_info_table

---
+++ Database/table_join.sql
@@ -0,0 +1,19 @@
+SELECT aq._c0, 
+	aq.cbsa_code,
+    aq.date,
+    site.number_of_sites_reporting,
+    site.city_ascii,
+    site.state_id,
+    site.state_name,
+	site.lat,
+	site.lng,
+	pop.population,
+	pop.density
+INTO site_info_table
+FROM air_quality_table AS aq
+LEFT JOIN site_reporting_table AS site
+ON aq._c0 = site._c0
+LEFT JOIN population_table AS pop
+ON aq._c0 = pop._c0
+
+
